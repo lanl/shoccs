d1485e589c521c0c56ba3f6a34d5dd08b3a3ebe9
Peter Brady <ptb@.lanl.gov>
2021-03-17 09:06:25 -0600

    Removed old tuple

--- src/mesh/mesh_view.t.cpp
+++ src/mesh/mesh_view.t.cpp
@@ -3,7 +3,6 @@
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-#include "fields/result_field.hpp"
 #include <range/v3/range/conversion.hpp>
 #include <range/v3/view/zip.hpp>
 
